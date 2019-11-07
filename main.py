1dd4c21cb6b05713a0f47a36acbefa9f81afd6ba
Piotr Francug <piotr.francug@gmail.com>
2019-11-07 11:56:18 +0100

    modulo wiadomosc

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ dlugoscTekstuJawnego = len(tekstJawny)
 
 dlugoscKlucza = None
 
+# Sprawdzenie dlugosci klucza
 while dlugoscKlucza != 4:
     print('Klucz musi posiadać dokładnie 4 znaki!')
     klucz = input('Podaj klucz: ')
     dlugoscKlucza = len(klucz)
 
-print(klucz)
+# Uzupelnienie dlugosci tekstu jawnego
+# do wielokrotnosci 8 znakow (64 bit)
+
+if dlugoscTekstuJawnego % 8:
+    print(dlugoscTekstuJawnego % 8)
