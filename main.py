300a8fc0a0c4fa155dcd3d8fc3dc1a1cfe1044d4
Piotr Francug <piotr.francug@gmail.com>
2019-11-07 14:43:52 +0100

    uzupelnienie do 8

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-tekstJawny = input('Podaj tekst jawny: ')
-dlugoscTekstuJawnego = len(tekstJawny)
+dlugoscTekstuJawnego = 0
 
-dlugoscKlucza = None
+while dlugoscTekstuJawnego == 0:
+    tekstJawny = input('Podaj tekst jawny: ')
+    dlugoscTekstuJawnego = len(tekstJawny)
 
+dlugoscKlucza = 0
 # Sprawdzenie dlugosci klucza
 while dlugoscKlucza != 4:
     print('Klucz musi posiadać dokładnie 4 znaki!')
@@ -11,6 +13,12 @@ while dlugoscKlucza != 4:
 
 # Uzupelnienie dlugosci tekstu jawnego
 # do wielokrotnosci 8 znakow (64 bit)
-
 if dlugoscTekstuJawnego % 8:
-    print(dlugoscTekstuJawnego % 8)
+    for i in range(dlugoscTekstuJawnego % 8, 8):
+        # tekstJawny = tekstJawny + '0'
+        tekstJawny += '0'
+    dlugoscTekstuJawnego = len(tekstJawny)
+
+# konwersja tekstu jawnego na bity
+tablicaBitowJawnych = []
+
