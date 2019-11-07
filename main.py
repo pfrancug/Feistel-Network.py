f6f4f75d7fb0c6b866185456a5f3a251232c51f9
Piotr Francug <piotr.francug@gmail.com>
2019-11-07 23:05:48 +0100

    pretty much done

--- main.py
+++ main.py
@@ -1,53 +1,88 @@
-# wprowadzenie danych
-dlugoscTekstuJawnego = 0
-while dlugoscTekstuJawnego == 0:
+# -------------------------------------
+# -------- wprowadzenie danych --------
+# -------------------------------------
+
+# wprowadzenie tekstu jawnego
+tekstJawny = ''
+while len(tekstJawny) == 0:
     tekstJawny = input('Podaj tekst jawny: ')
-    dlugoscTekstuJawnego = len(tekstJawny)
 
 # wprowadzenie klucza
-dlugoscKlucza = 0
-while dlugoscKlucza != 4:
-    print('Klucz musi posiadać dokładnie 4 znaki!')
-    klucz = input('Podaj klucz: ')
-    dlugoscKlucza = len(klucz)
+klucz = ''
+while len(klucz) != 4:
+    klucz = input('Podaj 4-znakowy klucz szyfrujący: ')
 
-# uzupelnienie dlugosci tekstu jawnego
-# do wielokrotnosci 8 znakow (64 bit)
-if dlugoscTekstuJawnego % 8:
-    for i in range(dlugoscTekstuJawnego % 8, 8):
+# ------------------------------------------
+# -------- konwersja danych na bity --------
+# ------------------------------------------
+
+# uzupełnienie długości tekstu jawnego do wielokrotności ósemki
+if len(tekstJawny) % 8:
+    for i in range(len(tekstJawny) % 8, 8):
         tekstJawny += '0'
-    dlugoscTekstuJawnego = len(tekstJawny)
 
 # konwersja tekstu jawnego na bity
-tablicaBitowJawnych = []
-for i in range(0, dlugoscTekstuJawnego):
+postacBinarnaTekstuJawnego = []
+for i in range(0, len(tekstJawny)):
     kodASCII = ord(tekstJawny[i])
     osiemBit = bin(kodASCII)[2:].zfill(8)
-    tablicaBitowJawnych += osiemBit
+    postacBinarnaTekstuJawnego += list(map(int, osiemBit))
 
 # konwersja klucza na bity
-tablicaBitowKlucza = []
+postacBinarnaKlucza = []
 for i in range(0, 4):
     kodASCII = ord(klucz[i])
     osiemBit = bin(kodASCII)[2:].zfill(8)
-    tablicaBitowKlucza += list(map(int,osiemBit))
+    postacBinarnaKlucza += list(map(int, osiemBit))
 
-# podzial na bloki 64 i 32 bit
-bloki = []
-for i in range(0, len(tablicaBitowJawnych), +64):
-    lewa = list(map(int,tablicaBitowJawnych[i:i+32]))
-    prawa = list(map(int,tablicaBitowJawnych[i+32:i+64]))
-    bloki.append([lewa, prawa])
+# -------------------------------------
+# -------- podział na bloki -----------
+# -------------------------------------
 
-# szyfrowanie
-zaszyfrowaneBloki = []
-for i in range(0,len(bloki)):
-    lewa = bloki[i][0]
-    prawa = bloki[i][1]
-    lewaPrim = prawa
-    # P' = L XOR ( K AND P )
-    AND = list(map(lambda n1, n2: n1 & n2, tablicaBitowKlucza, prawa))
-    prawaPrim = list(map(lambda n1, n2: n1 ^ n2, lewa, AND))
-    
-    print(lewa, prawa)
-    print(lewaPrim, prawaPrim)
+blokiTekstuJawnego = []
+for i in range(0, len(postacBinarnaTekstuJawnego), +64):
+    lewa = postacBinarnaTekstuJawnego[i:i + 32]
+    prawa = postacBinarnaTekstuJawnego[i + 32:i + 64]
+    blokiTekstuJawnego.append([lewa, prawa])
+
+# -----------------------------
+# -------- szyfrowanie --------
+# -----------------------------
+
+szyfrowaneBloki = blokiTekstuJawnego
+for i in range(0, 2):
+    for j in range(0, len(szyfrowaneBloki)):
+        lewa = szyfrowaneBloki[j][0]
+        prawa = szyfrowaneBloki[j][1]
+        lewaPrim = prawa
+        # P' = L XOR ( K AND P )
+        funkcjaAND = list(map(lambda n1, n2: n1 & n2,
+                              postacBinarnaKlucza, prawa))
+        prawaPrim = list(map(lambda n1, n2: n1 ^ n2, lewa, funkcjaAND))
+        szyfrowaneBloki[j][0] = lewaPrim
+        szyfrowaneBloki[j][1] = prawaPrim
+
+# -----------------------------------------
+# -------- konwersja bitów na dane --------
+# -----------------------------------------
+
+# połączenie zaszyfriwanych bloków
+zaszyfrowanaPostacBinarna = ''
+for blok64 in range(0, len(szyfrowaneBloki)):
+    ciag64bitow = ''
+    for blok32 in range(0, len(szyfrowaneBloki[blok64])):
+        ciag64bitow += ''.join(map(str, szyfrowaneBloki[blok64][blok32]))
+    zaszyfrowanaPostacBinarna += ciag64bitow
+
+# konwersja zaszyfrowanych bitów na tekst
+szyfrogram = ''
+for i in range(0, len(zaszyfrowanaPostacBinarna), +8):
+    kodASCII = int(zaszyfrowanaPostacBinarna[i:i+8], 2)
+    znak = chr(kodASCII)
+    szyfrogram += znak
+
+# ------------------------------------------
+# -------- wyświetlenie szyfrogramu --------
+# ------------------------------------------
+
+print(szyfrogram)
