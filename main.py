4ccf2ce882abf035537937fbe75141288853a884
Piotr Francug <piotr.francug@gmail.com>
2019-11-07 11:45:56 +0100

    klucz

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print('test')
-print('wojtek')
-print('dupa')
-print("jasny chuj")
+tekstJawny = input('Podaj tekst jawny: ')
+dlugoscTekstuJawnego = len(tekstJawny)
+
+dlugoscKlucza = None
+
+while dlugoscKlucza != 4:
+    print('Klucz musi posiadać dokładnie 4 znaki!')
+    klucz = input('Podaj klucz: ')
+    dlugoscKlucza = len(klucz)
+
+print(klucz)
