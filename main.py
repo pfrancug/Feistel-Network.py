7491e9abf5afd7e88a4894784a6f632d5cf2b942
Piotr Francug <piotr.francug@gmail.com>
2019-11-08 11:42:09 +0100

    piateczek

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ klucz =''
 while len(klucz) != 4:
     klucz = input('Podaj 4-znakowy klucz: ')
 
-# Zamiana tekstu jawnego na bity
-ciagBitowTekstuJawnego = ''
-for znak in tekstJawny:
-    kodASCII = ord(znak)
-    kodBinarny = bin(kodASCII)[2:].zfill(8)
-    ciagBitowTekstuJawnego += kodBinarny
+# funkcja zamieniajaca ciag znakow na bity
+def zamianaTekstuNaBity(tekstDoZmiany):
+    ciagBitow = ''
+    for znak in tekstDoZmiany:
+        kodASCII = ord(znak)
+        kodBinarny = bin(kodASCII)[2:].zfill(8)
+        ciagBitow += kodBinarny
+    return ciagBitow
 
+# Zamiana tekstu jawnego i klucza na bity
+ciagBitowTekstuJawnego = zamianaTekstuNaBity(tekstJawny)
 ciagBitowTekstuJawnego += '0' * (64 - len(ciagBitowTekstuJawnego) % 64)
+ciagBitowKlucza = zamianaTekstuNaBity(klucz)
 
 # funkcja feistela
-def dodawanie(a, b):
-    
+def funkcjaFeistela(ciagBitowTekstuJawnego, ciagBitowKlucza):
+    ciagBitowTekstuZaszyfrowanego = '01' * 32
+
+    return ciagBitowTekstuZaszyfrowanego
+
+zaszyfrowaneBity = funkcjaFeistela(ciagBitowTekstuJawnego, ciagBitowKlucza)
+print(zaszyfrowaneBity)
